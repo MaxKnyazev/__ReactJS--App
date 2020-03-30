e76b3fde15ae3ab3cc6691516605ebba4e86fd54
MaxKnyazev <max.knyazev77@gmail.com>
2020-03-30 16:52:35 +0300

    2020.03.30 New components...

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TodoList from './components/TodoList';
-import AppHeader from './components/AppHeader';
-import SearchPanel from './components/SearchPanel';
+import './index.css';
+import App from './components/App';
 
-const App = () => {
-  const todoData = [
-    {id : 1, label : 'Drink Cofee', important : false},
-    {id : 2, label : 'Create React App', important : true},
-    {id : 3, label : 'Have a luch', important : false},
-  ]
-
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos = {todoData}/>
-    </div>
-  )
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,
+  document.getElementById('root'));
